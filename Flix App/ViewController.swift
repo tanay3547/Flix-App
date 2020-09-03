0c40bbdb7d58b3b4f4befe885a0ebf46b487ae47
tanay3547 <49569156+tanay3547@users.noreply.github.com>
2020-09-04 02:31:37 +0530

    Initial Commit

---
+++ Flix App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix App
+//
+//  Created by Tanay Agrawal on 9/4/20.
+//  Copyright © 2020 Tanay Agrawwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
